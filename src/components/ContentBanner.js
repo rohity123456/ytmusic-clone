2994048f78e339cb5dd2a0a680b257ad96be9f76
Rohit Yadav <rohity123456@gmail.com>
2020-10-07 01:22:40 +0530

    added some more changes

--- src/components/ContentBanner.js
+++ src/components/ContentBanner.js
@@ -4,8 +4,10 @@ function ContentBanner({ imgUrl, title, info }) {
   return (
     <div className="contentBanner" style={{ background: `url(${imgUrl})` }}>
       <div className="bannerContent">
-        <h1>{title}</h1>
-        <h2>{info}</h2>
+        <h1 className="largeP">{title}</h1>
+        <div>
+          <h2>{info}</h2>
+        </div>
       </div>
     </div>
   );
